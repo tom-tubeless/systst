e82bda2bc4cba06218b93f3cc3deb0b3658f5773
tom_tubeless <lutz.tomala@gmail.com>
2021-11-30 12:21:37 +0100

    moved to classic config

--- configuration.nix
+++ configuration.nix
@@ -2,14 +2,14 @@
 
 {
 
-  imports =
-    [ ./hardware-configuration.nix ];
+  imports = [
+    ./services/nixos-auto-update.nix
+  ];
 
   fileSystems."/" = { options = [ "noatime" "nodiratime" ]; };
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-
     loader = {
       efi.canTouchEfiVariables = true;
       grub = {
@@ -20,7 +20,6 @@
         device = "nodev";
       };
     };
-
     initrd.luks.devices = {
       crypt = {
         device = "/dev/sda2";
@@ -29,12 +28,61 @@
     };
   };
 
-  time.timeZone = "Europe/Berlin";
-
   networking = {
     useDHCP = false;
-    interfaces.enp0s3.useDHCP = true;
+    interfaces.enp0s3.useDHCP = true; # Add correct network interface name to find out run "ip a"
     hostName = "nixtst"; # Define your hostname.
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 443 2022 ];
+      allowedUDPPorts = [ 53 ];
+      allowPing = true;
+    };
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n = {
+    defaultLocale = "de_DE.UTF-8";
+    supportedLocales = [ "de_DE.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+  };
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      powerline-fonts
+      nerdfonts
+    ];
+  };
+
+  services = {
+    nixos-auto-update.enable = true;
+    logrotate = {
+      enable = true;
+      extraConfig = ''
+        compress
+        create
+        daily
+        dateext
+        delaycompress
+        missingok
+        notifempty
+        rotate 31
+      '';
+    };
+    openssh = {
+      enable = true;
+      permitRootLogin = "no";
+      passwordAuthentication = false;
+      forwardX11 = true;
+      ports = [ 2022 ];
+    };
   };
 
   security = {
@@ -44,114 +92,25 @@
     };
   };
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "en_US.UTF-8/UTF-8" ];
-  };
-
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
-
-  fonts = {
-    fontDir.enable = true;
-    enableGhostscriptFonts = true;
-
-    fonts = with pkgs; [
-      powerline-fonts
-      nerdfonts
-    ];
-  };
-
   users = {
     mutableUsers = false;
-    users.mudrii = {
+    users.nixu = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "docker" ];
+      extraGroups = [ "wheel" ];
+      shell = pkgs.fish;
       # mkpasswd -m sha-512 password
-      hashedPassword = "$6$428HYMOb$oCi6HhwWWgBqFTUkFlDqvjeiON4ETfPXYxzh5kZwwJZP.XMHgG2jNipFN3ulrwgfl7fW7YhJJBcYQavh7F7S41";
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8E9qXTKVGPUVFynznaYEUwXCEyjNjE/CYmlciqKpmh tom1@xps-15" ];
-
+      hashedPassword = "$6$mUf6Czttpe9LEwhV$Q8KYWiWyjdKVh4msFQWifpAWltINjkPPOmTn9Q6RbZBEc4OEfQ07LDThm4Ov5Lieikl0eXi3KY8jEX1Jpt.oH0";
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEibeHYufNMwEcpXtSHXTbEvmW8Mla1v1W8YS/gacfbA tom1@xps-15" ];
       packages = with pkgs; [
-        python38Full
-        (
-          python3.withPackages (
-            ps: with ps; [
-              #poetry
-              pip
-              powerline
-              pygments
-              pygments-markdown-lexer
-              xstatic-pygments
-              pylint
-              numpy
-              pynvim
-            ]
-          )
-        )
+        python39Full
       ];
     };
   };
 
-  environment = {
-    systemPackages = with pkgs; [
-      gitAndTools.gitFull
-      google-cloud-sdk-gce
-      pulumi-bin
-      kubernetes
-      kubernetes-helm
-      kubeseal
-      helmfile
-      helmsman
-      kind
-      kube3d
-      argo
-      argocd
-      kustomize
-      k9s
-      kubectx
-      jq
-      binutils
-      gnutls
-      wget
-      curl
-      htop
-      bind
-      mkpasswd
-      trash-cli
-      exa
-    ];
-
-    shellAliases = {
-      cp = "cp -i";
-      diff = "diff --color=auto";
-      dmesg = "dmesg --color=always | lless";
-      egrep = "egrep --color=auto";
-      fgrep = "fgrep --color=auto";
-      grep = "grep --color=auto";
-      la = "exa -alg --group-directories-first -s=type --icons";
-      lless = "set -gx LESSOPEN '|pygmentize -f terminal256 -g -P style=monokai %s' && set -gx LESS '-R' && less -m -g -i -J -u -Q";
-      ll = "exa -la";
-      ls = "exa";
-      mv = "mv -i";
-      ping = "ping -c3";
-      ps = "ps -ef";
-      rm = "trash-put";
-      unrm = "trash-restore";
-      rmcl = "trash-empty";
-      rml = "trash-list";
-      sudo = "sudo -i";
-      vdir = "vdir --color=auto";
-    };
-  };
-
   programs = {
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
+    ssh.startAgent = false;
     vim.defaultEditor = true;
+    fish.enable = true;
     nano.nanorc = ''
       unset backup
       set nonewlines
@@ -162,25 +121,37 @@
     '';
   };
 
-  virtualisation = {
-    docker = {
-      enable = true;
-      autoPrune.enable = true;
-      enableOnBoot = true;
-    };
-  };
+  environment = {
+    systemPackages = with pkgs; [
+      git
+      gh
+      inotify-tools
+      nodejs
+      binutils
+      gnutls
+      wget
+      curl
+      bind
+      mkpasswd
+      cachix
+    ];
 
-  services = {
-    lorri.enable = true;
-    openssh = {
-      enable = true;
-      permitRootLogin = "no";
-      passwordAuthentication = false;
+    shellAliases = {
+      cp = "cp -i";
+      diff = "diff --color=auto";
+      dmesg = "dmesg --color=always | lless";
+      egrep = "egrep --color=auto";
+      fgrep = "fgrep --color=auto";
+      grep = "grep --color=auto";
+      mv = "mv -i";
+      ping = "ping -c3";
+      ps = "ps -ef";
+      sudo = "sudo -i";
+      vdir = "vdir --color=auto";
     };
   };
 
   nixpkgs = {
-    overlays = [ overlay-unstable ];
     config = {
       allowBroken = true;
       allowUnfree = true;
@@ -194,23 +165,39 @@
     readOnlyStore = false;
     allowedUsers = [ "@wheel" ];
     trustedUsers = [ "@wheel" ];
-
     extraOptions = ''
       experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
     '';
-
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 7d --max-freed $((64 * 1024**3))";
     };
-
     optimise = {
       automatic = true;
       dates = [ "weekly" ];
     };
+    /*    binaryCaches = [
+      "https://matrix.cachix.org"
+      ];
+      binaryCachePublicKeys = [
+      "matrix.cachix.org-1:h2ZM1LtvJBQhCb7a2Z/UpO8PKKIUlIvifvrFKfnHkro="
+      ];*/
   };
-
-  system.stateVersion = "21.05"; # Did you read the comment?
-
+  system = {
+    stateVersion = "21.05"; # Did you read the comment?
+    autoUpgrade = {
+      enable = true;
+      allowReboot = true;
+      flake = "github:mudrii/systst";
+      flags = [
+        "--recreate-lock-file"
+        "--no-write-lock-file"
+        "-L" # print build logs
+      ];
+      dates = "daily";
+    };
+  };
 }
